293c57ebe680daa94b4d38d4e8a223fcf32464a3
NataVP4 <natalia.vidal.piqueras@ces-vegamedia.es>
2024-03-10 22:46:39 +0100

    Practicando.

---
+++ Array3/Ejercicio_01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v1 = new int[2];
+            int[] v2 = new int[2];
+            int[] v3 = new int[2];
+            String linea;
+
+            Console.WriteLine("1ºVECTOR: ");
+            for (int i = 0; i < v1.Length; i++)
+            {
+                Console.Write("Valor posición ["+(i+1)+"]: ");
+                linea= Console.ReadLine();
+                v1[i] = int.Parse(linea);
+            }
+            Console.WriteLine("2ºVECTOR: ");
+            for (int i = 0; i < v2.Length; i++)
+            {
+                Console.Write("Valor posición [" + (i + 1) + "]: ");
+                linea = Console.ReadLine();
+                v2[i] = int.Parse(linea);
+            }
+
+            Console.WriteLine("3ºVECTOR: ");
+            for (int i = 0; i < v3.Length; i++)
+            {
+                v3[i] = v1[i] + v2[i];
+                Console.Write(v3[i]+" ");
+            }                   
+
+        }
+    }
+}
---
+++ Array3/Ejercicio_03/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Ejercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*3. Se tienen las notas del primer parcial de los alumnos de dos cursos, el curso A y el curso B,
+             * cada curso cuenta con 5 alumnos. Realizar un programa que muestre el curso que obtuvo el mayor promedio general.*/
+
+            float[] vectorA = new float[5];
+            float[] vectorB = new float[5];
+            String linea;
+            float sumA=0, sumaB=0;
+            float promedioA = 0, promedioB=0;
+
+            Console.WriteLine("Notas grupo A: ");
+            for (int i = 0; i < vectorA.Length; i++)
+            {
+                Console.Write("[" + (i + 1) + "] Nota: ");
+                linea = Console.ReadLine();
+                vectorA[i] = float.Parse(linea);
+                sumA += vectorA[i];
+
+            }
+            promedioA = sumA / vectorA.Length;
+            Console.WriteLine("Promedio grupo A: " + promedioA);
+
+            Console.WriteLine("Notas grupo B: ");
+            for (int i = 0; i < vectorA.Length; i++)
+            {
+                Console.Write("[" + (i + 1) + "] Nota: ");
+                linea = Console.ReadLine();
+                vectorB[i] = float.Parse(linea);
+                sumaB += vectorB[i];
+            }
+            promedioB = sumaB / vectorB.Length;
+            Console.WriteLine("Promedio grupo B: " + promedioB);
+
+            if (promedioA > promedioB)
+            {
+                Console.WriteLine("El promedio del grupo A es mayor.");
+            }
+            else if (promedioB > promedioA)
+            {
+                Console.WriteLine("El promedio del grupo B es mayor.");
+            }
+            else
+            {
+                Console.WriteLine("Los promedios de ambos grupos son iguales.");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
